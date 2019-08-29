7b79e8a0eb93966fd8ba1f1e7da06195c04e62cd
Patrick Lohan <patrick.lohan@zoho.com>
2019-08-29 13:30:44 +0100

    Sets up GChart

--- src/components/EnergyChart.vue
+++ src/components/EnergyChart.vue
@@ -1,9 +1,24 @@
 <template lang="html">
-  
+  <GChart
+    type="ColumnChart"
+    :data="chartData"
+    :options="chartOptions"
 </template>
 
 <script>
+import { GChart } from 'vue-google-charts'
+
 export default {
+  name: 'energy-chart',
+  data(){
+    return {
+      chartData: [],
+      chartOptions: {}
+    }
+  }
+  components: {
+    GChart
+  }
 }
 </script>
 
